641ce210c2680a3e4a9131e890e2de35feda9672
Gustavo <gustavo.alt1@gmail.com>
2021-06-11 19:00:57 -0400

    commit tangle associated with org file

--- lib_oldpme.r
+++ lib_oldpme.r
@@ -32,7 +32,22 @@ oldpmeread <- function(unit = "", timeframe = "", pme_dir = .pme_dir) {
   return(ldata)
 }
 
+oldpme_flag_bad_state <- function(hh_old, flagname = ".flbadstate") {
+  valid_states_V10 <- c(11, 21, 33, 41, 56, 59)
+  valid_states_UF <- c(26, 29, 31, 33, 35, 43)
+
+  hh_old[!is.na(V10), (flagname) := !(V10 %in% valid_states_V10)]
+  hh_old[!is.na(UF), (flagname) := !(UF %in% valid_states_UF)]
+
+}
+
 generate_key <- function(dthh, keyvars = c("V10", "V101", "V102", "V103", "V106")) {
-  Reduce(function(x,y) paste(x, y, sep = "-"),
-         dthh[, keyvars, with = FALSE])
+  vkey <- Reduce(function(x,y) paste(x, y, sep = "-"),
+                 dthh[, keyvars, with = FALSE])
+  vna <- Reduce(function(x,y) x | y,
+                lapply(dthh[, keyvars, with = FALSE], is.na))
+
+  vkey[vna] <- NA_character_
+
+  return(vkey)
 }
